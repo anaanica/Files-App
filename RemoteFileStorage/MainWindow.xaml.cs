0e7fce6f8260e2c2c6976ded3f8792de06b72b77
anaanica <anica3220@gmail.com>
2024-06-13 14:35:58 +0200

    project commit

---
+++ RemoteFileStorage/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using Azure.Storage.Blobs.Models;
+using Microsoft.Win32;
+using RemoteFileStorage.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RemoteFileStorage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly ItemsViewModel itemsViewModel;
+        public MainWindow()
+        {
+            InitializeComponent();
+            itemsViewModel = new ItemsViewModel();
+            Init();
+        }
+
+        private void Init()
+        {
+            cbDirectories.ItemsSource = itemsViewModel.Directories;
+            lbItems.ItemsSource = itemsViewModel.Items;
+        }
+
+        private void LbItems_SelectionChanged(object sender, SelectionChangedEventArgs e)
+         => DataContext = lbItems.SelectedItem as BlobItem;
+
+        private void CbDirectories_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                itemsViewModel.Directory = cbDirectories.Text.Trim();
+                cbDirectories.Text = itemsViewModel.Directory;
+            }
+        }
+
+        private void CbDirectories_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (itemsViewModel.Directories.Contains(cbDirectories.Text))
+            {
+                itemsViewModel.Directory = cbDirectories.Text;
+                cbDirectories.SelectedItem = itemsViewModel.Directory;
+            }
+        }
+        private async void BtnUpload_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files|*.jpeg; *.tiff; *.png; *.svg; *.gif;";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                await itemsViewModel.UploadAsync(openFileDialog.FileName/*, cbDirectories.Text*/);
+            }
+            cbDirectories.Text = itemsViewModel.Directory;
+        }
+
+        private async void BtnDownload_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbItems.SelectedItem is not BlobItem item)
+            {
+                return;
+            }
+            var saveFileDialog = new SaveFileDialog()
+            {
+                FileName = item.Name[(item.Name.LastIndexOf(ItemsViewModel.ForwardSlash) + 1)..]
+            };
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                await itemsViewModel.DownloadAsync(item, saveFileDialog.FileName);
+            }
+            cbDirectories.Text = itemsViewModel.Directory;
+
+        }
+
+        private async void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbItems.SelectedItem is not BlobItem item)
+            {
+                return;
+            }
+            await itemsViewModel.DeleteAsync(item);
+            cbDirectories.Text = itemsViewModel.Directory;
+        }
+    }
+}
